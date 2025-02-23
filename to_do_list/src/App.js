a15ef656d2bc181ced1838fc6ae3d01cfce0c232
IrekM <12935670+irekM@users.noreply.github.com>
2025-02-23 01:16:37 +0100

    add first basic structures with two components

---
+++ to_do_list/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import SearchForm from "./components/SearchForm/SearchForm";
+import List from "./components/List/List";
+
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+      <List />
+    </div>
+  );
+};
+
+export default App;
+
+
