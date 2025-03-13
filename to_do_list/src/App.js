86b0b5b306558cc194012de688ff38664a90e0ed
IrekM <12935670+irekM@users.noreply.github.com>
2025-03-13 02:04:30 +0100

    install fontawsome for react and create styles for navBar

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -3,15 +3,19 @@ import Hero from "./components/Hero/Hero";
 import SearchForm from "./components/SearchForm/SearchForm";
 import List from "./components/List/List";
 import Container from "./Container/Container";
+import NavBar from "./components/NavBar/NavBar";
 
 
 const App = () => {
   return (
+    <main>
+      <NavBar/>
       <Container>
         <Hero />
         <SearchForm />
         <List />
       </Container>
+    </main>
   );
 };
 
---
+++ to_do_list/src/components/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from './navBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';  // Import głównego komponentu
+import { faTasks } from '@fortawesome/free-solid-svg-icons';  // Import konkretnej ikony "tasks"
+import Container from "../../Container/Container";
+
+const NavBar = () => {
+
+    return (
+        <nav className={styles.navbar}>
+      <Container>
+        {/* Link with FontAwesome Icon */}
+        <a href="/" className={styles.iconLink}>
+          <FontAwesomeIcon icon={faTasks} className={styles.icon} />
+        </a>
+
+        {/* Navigation Links */}
+        <ul className={styles.navList}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/favorite">Favorite</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </Container>
+    </nav>
+    );
+}
+
+export default NavBar;
