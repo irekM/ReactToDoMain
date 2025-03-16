67a0237d1d3ba1fc27cdecfaaede17122d51f4cb
IrekM <12935670+irekM@users.noreply.github.com>
2025-03-16 16:26:40 +0100

    add linkto component against normal a link

--- to_do_list/src/components/NavBar/NavBar.js
+++ to_do_list/src/components/NavBar/NavBar.js
@@ -2,7 +2,9 @@ import React from "react";
 import styles from './NavBar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';  // Import głównego komponentu
 import { faTasks } from '@fortawesome/free-solid-svg-icons';  // Import konkretnej ikony "tasks"
+import { Link } from "react-router-dom";
 import Container from "../../Container/Container";
+
 
 const NavBar = () => {
 
@@ -18,9 +20,9 @@ const NavBar = () => {
                 <FontAwesomeIcon icon={faTasks} className={styles.icon} />
             </a>
 
-          <li><a href="/">Home</a></li>
-          <li><a href="/favorite">Favorite</a></li>
-          <li><a href="/about">About</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/favorite">Favorite</Link></li>
+          <li><Link to="/about">About</Link></li>
         </ul>
       </Container>
     </nav>
