ddc466f1d70171c2ca88e7fa5c15c6aa610a472d
IrekM <12935670+irekM@users.noreply.github.com>
2025-03-16 16:09:35 +0100

    add notfound404 component with route

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import Favorite from "./components/Favorite/Favorite";
 import About from "./components/About/About";
+import NotFound from "./components/NotFoundComponent/NotFound";
 
 
 
@@ -17,6 +18,7 @@ const App = () => {
           <Route path="/" element={<Home/>} />
           <Route path="/about" element ={<Favorite/>} />
           <Route path="/favorite" element={<About />} />
+          <Route path="*" element={<NotFound/>} />
         </Routes>
       </Container>
     </main>
---
+++ to_do_list/src/components/NotFoundComponent/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PageTitle from "../PageTitle/PageTitle";
+
+
+const NotFound = (props) => {
+    return (
+        <div>
+            <PageTitle>404 Not Found</PageTitle>
+        </div>
+    );
+};
+
+export default NotFound;
