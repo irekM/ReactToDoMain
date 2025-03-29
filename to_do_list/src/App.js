bd8dfcfbbc825e82c32ad2abebd5cda86e557107
IrekM <12935670+irekM@users.noreply.github.com>
2025-03-29 12:36:11 +0100

    create route path list based on parameter listId syntax

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import Favorite from "./components/Favorite/Favorite";
 import About from "./components/About/About";
+import List from "./components/List/List";
 import NotFound from "./components/NotFoundComponent/NotFound";
 
 
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/" element={<Home/>} />
           <Route path="/about" element ={<Favorite/>} />
           <Route path="/favorite" element={<About />} />
+          <Route path="/list/:listId" element={<List/>}/>
           <Route path="*" element={<NotFound/>} />
         </Routes>
       </Container>
