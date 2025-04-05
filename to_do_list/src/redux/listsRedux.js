c9dbdf562aaecc49c98266947b253396c0bd0975
IrekM <12935670+irekM@users.noreply.github.com>
2025-04-05 13:53:23 +0200

    create external subreducer file for listsRedux

--- to_do_list/src/redux/listsRedux.js
+++ to_do_list/src/redux/listsRedux.js
@@ -1,6 +1,21 @@
 import shortid from "shortid";
 
-const listReducer = (statePart = [], action) => {
+//selectrors
+
+export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
+export const getAllLists = ({lists}) => lists;
+
+
+//actions
+const createActionName = actionName => `app/lists/${actionName}`;
+const ADD_LIST = createActionName('ADD_LIST');
+
+//action creator
+export const addList = payload => ({type: ADD_LIST, payload});
+
+
+const listsReducer = (statePart = [], action) => {
+
     switch(action.type){
       case 'ADD_LIST':
         return [...statePart, {...action.payload, id: shortid()}];
@@ -9,4 +24,4 @@ const listReducer = (statePart = [], action) => {
     }
   }
 
-  export default listReducer;
+  export default listsReducer;
--- to_do_list/src/redux/store.js
+++ to_do_list/src/redux/store.js
@@ -12,11 +12,9 @@ export const getFilteredCards = ({ cards, searchString }, columnId) => cards
   .filter(card => card.columnId === columnId && strContains(card.title, searchString));
 
 export const getAllColumns = state => state.columns;
-export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
 export const getColumnsByList = ({ columns }, listId) => 
   columns.filter(column => column.listId === listId);
 
-export const getAllLists = state => state.lists;
 export const getFavoriteCards = state => state.cards.filter(card => card.isFavorite);
 
   // action creators
@@ -24,7 +22,6 @@ export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRIG', payload});
 export const toggleCardFavorite = payload =>  ({ type: 'TOGGLE_CARD_FAVORITE', payload });
-export const addList = payload => ({type: 'ADD_List', payload});
 
 //zmiana reducera na subreducery
 
