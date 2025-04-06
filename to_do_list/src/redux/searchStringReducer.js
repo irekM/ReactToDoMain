7dada814f35db4f08b419acc93ca690b54380419
IrekM <12935670+irekM@users.noreply.github.com>
2025-04-07 01:19:52 +0200

    add externall files for subreducers with actions and selectors

--- to_do_list/src/redux/searchStringReducer.js
+++ to_do_list/src/redux/searchStringReducer.js
@@ -1,4 +1,16 @@
 
+// selector
+export const getSearchString = state => state.searchString;
+
+//actions
+const createActionName = actionName => `app/CARD/${actionName}`;
+const UPDATE_SEARCHSTRIG = createActionName('UPDATE_SEARCHSTRIG');
+
+
+//action creator
+export const updateSearchString = payload => ({ type: UPDATE_SEARCHSTRIG, payload});
+
+
 const searchStringReducer = (statePart = '', action) => {
     switch(action.type) {
       case 'UPDATE_SEARCHSTRING':
--- to_do_list/src/redux/store.js
+++ to_do_list/src/redux/store.js
@@ -1,27 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import initialState from './initialState';
 // import shortid from 'shortid';
-import strContains from '../utils/strContains';
-import listReducer from './listsRedux';
+
+import listReducer from './listsReducer';
 import columnsReducer from './columnsReducer';
 import cardsReducer from './cardsReducer';
 import searchStringReducer from './searchStringReducer';
 
-//selectors
-export const getFilteredCards = ({ cards, searchString }, columnId) => cards
-  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
-
-export const getAllColumns = state => state.columns;
-export const getColumnsByList = ({ columns }, listId) => 
-  columns.filter(column => column.listId === listId);
-
-export const getFavoriteCards = state => state.cards.filter(card => card.isFavorite);
-
-  // action creators
-export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
-export const addCard = payload => ({ type: 'ADD_CARD', payload });
-export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRIG', payload});
-export const toggleCardFavorite = payload =>  ({ type: 'TOGGLE_CARD_FAVORITE', payload });
 
 //zmiana reducera na subreducery
 
